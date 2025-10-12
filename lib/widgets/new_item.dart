5af23fd17c40a8073b8fae4710eadea63323dc7a
dmakarau <denis.makarow@gmail.com>
2025-10-12 12:19:52 +0200

    Added fields and a dropdown to the add new item

--- lib/widgets/new_item.dart
+++ lib/widgets/new_item.dart
@@ -1,37 +1,69 @@
-
 import 'package:flutter/material.dart';
+import 'package:grocery_list/data/categories.dart';
 
 class NewItem extends StatefulWidget {
-
   const NewItem({super.key});
 
   @override
   State<NewItem> createState() {
     return _NewItemState();
   }
-
 }
 
 class _NewItemState extends State<NewItem> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Add New Item"),
-      ),
+      appBar: AppBar(title: const Text("Add New Item")),
       body: Padding(
         padding: const EdgeInsets.all(12),
         child: Form(
           child: Column(
             children: [
               TextFormField(
-                decoration: InputDecoration(
-                  label: const Text("Name"),
-                ),
+                decoration: InputDecoration(label: const Text("Name")),
                 maxLength: 50,
                 validator: (value) {
                   return "Test. TO BE DONE YET";
                 },
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        label: Text("Quantity"),
+                      ),
+                      initialValue: '1',
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  Expanded(
+                    child: DropdownButtonFormField(
+                      decoration: const InputDecoration(
+                        label: Text("Category"),
+                      ),
+                      items: [
+                        for (final category in categories.entries)
+                          DropdownMenuItem(
+                            value: category.value,
+                            child: Row(
+                              children: [
+                                Container(
+                                  width: 16,
+                                  height: 16,
+                                  color: category.value.color,
+                                ),
+                                const SizedBox(width: 6),
+                                Text(category.value.title),
+                              ],
+                            ),
+                          ),
+                      ],
+                      onChanged: (value) {},
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
